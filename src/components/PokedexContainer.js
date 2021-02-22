c32cb0bdcec4cc5881fd836f36e4b3433ea01bce
myelsoria <myrell.05@gmail.com>
2021-02-22 16:32:17 +0800

    added footer

--- src/components/PokedexContainer.js
+++ src/components/PokedexContainer.js
@@ -2,18 +2,22 @@ import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import Pokemons from './Pokemons'
 import Header from './Header'
+import Footer from './Footer'
 
 const PokedexContainer = () => {
-    const [searchText, setSearchText] = useState('')
-    const searchTextHandler = (data) => {
-        setSearchText(data)
-    }
-    return (
-        <Container>
-            <Header onSearchTextHandler={searchTextHandler} />
-            <Pokemons searchText={searchText} />
-        </Container>            
-    )
+  const [searchText, setSearchText] = useState('')
+  const searchTextHandler = (data) => {
+    setSearchText(data)
+  }
+  return (
+    <div>
+      <Container>
+        <Header onSearchTextHandler={searchTextHandler} />
+        <Pokemons searchText={searchText} />
+      </Container>
+      <Footer />
+    </div>
+  )
 }
 
 export default PokedexContainer
